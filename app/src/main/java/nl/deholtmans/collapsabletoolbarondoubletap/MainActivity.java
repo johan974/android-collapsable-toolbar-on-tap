acfef60725b6449ff4e591c933fbac66efffaf81
johan974 <jj974@upcmail.nl>
2019-08-22 21:06:36 +0200

    Improved version - that reacts on all kinds of gestures!

--- app/src/main/java/nl/deholtmans/collapsabletoolbarondoubletap/MainActivity.java
+++ app/src/main/java/nl/deholtmans/collapsabletoolbarondoubletap/MainActivity.java
@@ -13,94 +13,121 @@ import androidx.appcompat.widget.Toolbar;
 
 import com.google.android.material.appbar.AppBarLayout;
 
-public class MainActivity extends AppCompatActivity implements
-        GestureDetector.OnGestureListener,
-        GestureDetector.OnDoubleTapListener {
+public class MainActivity extends AppCompatActivity {
 
-    private final String TAG = MainActivity.class.getSimpleName();
-    private GestureDetector gestureDetector;
+    private GestureDetector mGestureDetector;
     private AppBarLayout appbar;
     private Toolbar toolbar;
     private FrameLayout content_frame;
     private TextView tv;
+    private boolean isExpanded = true;
+    static MainActivity mainActivity;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        mainActivity = this;
 
         setContentView(R.layout.activity_main);
 
-        gestureDetector = new GestureDetector(MainActivity.this, MainActivity.this);
-
+        // Setup toolbar AND a click means toggling the
         appbar = (AppBarLayout) findViewById(R.id.appbar);
         toolbar = (Toolbar) findViewById(R.id.toolbar);
+        toolbar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                isExpanded = false;
+                appbar.setExpanded(isExpanded);
+            }
+        });
 
         content_frame = (FrameLayout) findViewById(R.id.content_frame);
         LayoutInflater inflater = (LayoutInflater) MainActivity.this.getSystemService(LAYOUT_INFLATER_SERVICE);
         View framelayout_content = inflater.inflate(R.layout.content_main, null, false);
         content_frame.addView(framelayout_content);
         tv = (TextView) findViewById(R.id.tv);
-        tv.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                appbar.setExpanded(true);
-            }
-        });
 
-        toolbar.setOnTouchListener(new View.OnTouchListener() {
+        setupGestureDetector();
+        tv.setOnTouchListener(new View.OnTouchListener() {
             @Override
             public boolean onTouch(View view, MotionEvent motionEvent) {
-                MainActivity.this.gestureDetector.onTouchEvent(motionEvent);
-                return false;
+                mGestureDetector.onTouchEvent(motionEvent);
+                return true;
 
             }
         });
+    }
 
+    private void setupGestureDetector() {
+
+        mGestureDetector = new GestureDetector(this,
+
+                new GestureDetector.SimpleOnGestureListener() {
+                    @Override
+                    public boolean onDoubleTap(MotionEvent e) {
+                        UserFeedback.show("onDoubleTap");
+                        isExpanded = !isExpanded;
+                        appbar.setExpanded(isExpanded);
+                        return true;
+                    }
+
+                    @Override
+                    public boolean onDoubleTapEvent(MotionEvent e) {
+                        UserFeedback.show("onDoubleTapEvent");
+                        return true;
+                    }
+
+                    @Override
+                    public boolean onDown(MotionEvent e) {
+                        UserFeedback.show("onDown");
+                        return true;
+                    }
+
+                    @Override
+                    public boolean onFling(MotionEvent e1, MotionEvent e2,
+                                           float velocityX, float velocityY) {
+                        UserFeedback.show("onFling");
+                        return true;
+                    }
+
+                    @Override
+                    public void onLongPress(MotionEvent e) {
+                        UserFeedback.show("onLongPress");
+                    }
+
+                    @Override
+                    public boolean onScroll(MotionEvent e1, MotionEvent e2,
+                                            float distanceX, float distanceY) {
+                        UserFeedback.show("onScroll");
+                        return true;
+                    }
+
+                    @Override
+                    public void onShowPress(MotionEvent e) {
+                        UserFeedback.show("onShowPress");
+                    }
+
+                    @Override
+                    public boolean onSingleTapConfirmed(MotionEvent e) {
+                        UserFeedback.show("onSingleTapConfirmed");
+                        return true;
+                    }
+
+                    @Override
+                    public boolean onSingleTapUp(MotionEvent e) {
+                        UserFeedback.show("onSingleTapUp");
+                        return true;
+                    }
+                });
     }
 
     @Override
-    public boolean onSingleTapConfirmed(MotionEvent motionEvent) {
-        return false;
-    }
+    public boolean onTouchEvent(MotionEvent event) {
+        if (mGestureDetector != null) {
+            return mGestureDetector.onTouchEvent(event);
+        } else {
+            return super.onTouchEvent(event);
+        }
 
-    @Override
-    public boolean onDoubleTap(MotionEvent motionEvent) {
-        appbar.setExpanded(false);
-        return true;
-    }
-
-    @Override
-    public boolean onDoubleTapEvent(MotionEvent motionEvent) {
-        return true;
-    }
-
-    @Override
-    public boolean onDown(MotionEvent motionEvent) {
-        return true;
-    }
-
-    @Override
-    public void onShowPress(MotionEvent motionEvent) {
-
-    }
-
-    @Override
-    public boolean onSingleTapUp(MotionEvent motionEvent) {
-        return false;
-    }
-
-    @Override
-    public boolean onScroll(MotionEvent motionEvent, MotionEvent motionEvent1, float v, float v1) {
-        return false;
-    }
-
-    @Override
-    public void onLongPress(MotionEvent motionEvent) {
-
-    }
-
-    @Override
-    public boolean onFling(MotionEvent motionEvent, MotionEvent motionEvent1, float v, float v1) {
-        return false;
     }
 }
